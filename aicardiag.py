ae7d7dcc2bb9f2faabd2f74985b19d6e4ff8731e
Kharisma2751 <51434075+Kharisma2751@users.noreply.github.com>
2023-12-17 22:36:43 +0700

    Add files via upload

---
+++ aicardiag.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import google.generativeai as palm  # Replace with the correct import
+
+# Initialize Generative AI
+palm.configure(api_key="AIzaSyBdY9bOtYv0M_-xdj6gJShZfqjpuCjE92U")
+
+def initiate(user_input):
+    prompt = f"""
+    Analyzing Car Issues:
+    User Input: {user_input}
+    """
+
+    response = palm.chat(messages=prompt)
+    return response
+
+def reply(prev_msg, user_input):
+    prompt = f"""
+    Previous AI Response: {prev_msg.last}
+    User Input: {user_input}
+    """
+
+    return prev_msg.reply(prompt)
+
+def ai_car_diagnosis():
+    st.title("AI Car Diagnosis")
+
+    # Initial interaction
+    user_input = st.text_input("User Input:")
+    response = initiate(user_input)
+    st.text(f"AI: {response.last}")
+
+    # Continuing the conversation
+    user_input_list = [user_input]
+    form = st.form("user_inputs_form")
+
+    user_input = ""  # Initialize user_input outside the loop
+
+    while True:
+        user_input_list.append(user_input)
+        response = reply(response, user_input)
+
+        if user_input.lower() in ["exit", "quit", "stop"]:
+            st.text("Exiting AI Car Diagnosis.")
+
+    
+
+if __name__ == "__main__":
+    ai_car_diagnosis()
